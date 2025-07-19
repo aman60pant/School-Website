215c5d259fb78ae367f62c3d3570f81f969a42c3
Aman <aman60pant@gmail.com>
2025-07-19 16:47:37 +0530

    gallery page not fixed

--- Gallery.php
+++ Gallery.php
@@ -43,305 +43,38 @@
             </div>
         </div>
     </section>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-3">
-                <a href="assets/img/1.jpg" target="_parent">
-                    <img src="assets/img/1.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/2.jpg" target="_parent">
-                    <img src="assets/img/2.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/3.jpeg" target="_parent">
-                    <img src="assets/img/3.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/4.jpg" target="_parent">
-                    <img src="assets/img/4.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/5.jpg" target="_parent">
-                    <img src="assets/img/5.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/6.jpeg" target="_parent">
-                    <img src="assets/img/6.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/7.jpg" target="_parent">
-                    <img src="assets/img/7.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/8.jpg" target="_parent">
-                    <img src="assets/img/8.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/9.jpeg" target="_parent">
-                    <img src="assets/img/9.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/10.jpeg" target="_parent">
-                    <img src="assets/img/10.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/11.jpeg" target="_parent">
-                    <img src="assets/img/11.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/12.jpeg" target="_parent">
-                    <img src="assets/img/12.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/13.jpeg" target="_parent">
-                    <img src="assets/img/13.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/14.jpg" target="_parent">
-                    <img src="assets/img/14.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/15.jpeg" target="_parent">
-                    <img src="assets/img/15.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/16.jpg" target="_parent">
-                    <img src="assets/img/16.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/17.jpeg" target="_parent">
-                    <img src="assets/img/17.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/18.jpeg" target="_parent">
-                    <img src="assets/img/18.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/19.jpeg" target="_parent">
-                    <img src="assets/img/19.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/20.jpeg" target="_parent">
-                    <img src="assets/img/20.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/21.jpeg" target="_parent">
-                    <img src="assets/img/21.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/22.jpeg" target="_parent">
-                    <img src="assets/img/22.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/23.jpeg" target="_parent">
-                    <img src="assets/img/23.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/24.jpeg" target="_parent">
-                    <img src="assets/img/24.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/25.jpeg" target="_parent">
-                    <img src="assets/img/25.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/26.jpeg" target="_parent">
-                    <img src="assets/img/26.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/27.jpeg" target="_parent">
-                    <img src="assets/img/27.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/28.jpeg" target="_parent">
-                    <img src="assets/img/28.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/29.jpeg" target="_parent">
-                    <img src="assets/img/29.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/30.jpeg" target="_parent">
-                    <img src="assets/img/30.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/31.jpeg" target="_parent">
-                    <img src="assets/img/31.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/32.jpeg" target="_parent">
-                    <img src="assets/img/32.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/33.jpeg" target="_parent">
-                    <img src="assets/img/33.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/34.jpg" target="_parent">
-                    <img src="assets/img/34.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/35.jpeg" target="_parent">
-                    <img src="assets/img/35.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/36.jpeg" target="_parent">
-                    <img src="assets/img/36.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/37.jpg" target="_parent">
-                    <img src="assets/img/37.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/38.jpeg" target="_parent">
-                    <img src="assets/img/38.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/39.jpeg" target="_parent">
-                    <img src="assets/img/39.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/40.jpg" target="_parent">
-                    <img src="assets/img/40.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/41.jpg" target="_parent">
-                    <img src="assets/img/41.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/42.jpg" target="_parent">
-                    <img src="assets/img/42.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/43.jpg" target="_parent">
-                    <img src="assets/img/43.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/44.jpg" target="_parent">
-                    <img src="assets/img/44.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/45.jpeg" target="_parent">
-                    <img src="assets/img/45.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/46.jpg" target="_parent">
-                    <img src="assets/img/46.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/47.jpeg" target="_parent">
-                    <img src="assets/img/47.jpeg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/48.jpg" target="_parent">
-                    <img src="assets/img/48.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/49.jpg" target="_parent">
-                    <img src="assets/img/49.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/50.jpg" target="_parent">
-                    <img src="assets/img/50.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/51.jpg" target="_parent">
-                    <img src="assets/img/51.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/52.jpg" target="_parent">
-                    <img src="assets/img/52.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/53.jpg" target="_parent">
-                    <img src="assets/img/53.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/54.jpg" target="_parent">
-                    <img src="assets/img/54.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/55.jpg" target="_parent">
-                    <img src="assets/img/55.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/56.jpg" target="_parent">
-                    <img src="assets/img/56.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/57.jpg" target="_parent">
-                    <img src="assets/img/57.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/58.jpg" target="_parent">
-                    <img src="assets/img/58.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
-            <div class="col-lg-3">
-                <a href="assets/img/59.jpg" target="_parent">
-                    <img src="assets/img/59.jpg" alt="" class="gallery-img">
-                </a>
-            </div>
 
-        </div>
+    <!-- Gallery Thumbnails -->
+    <?php
+    $images = glob("assets/img/*.{jpg,jpeg,png,gif}", GLOB_BRACE);
+    ?>
+
+    <!-- Gallery Thumbnails -->
+    <div class="gallery">
+        <?php foreach ($images as $index => $img): ?>
+            <img src="<?= $img ?>" onclick="openLightbox();showSlide(<?= $index + 1 ?>)" class="thumbnail">
+        <?php endforeach; ?>
+    </div>
+
+    <!-- Lightbox Modal -->
+    <div id="lightboxModal" class="lightbox">
+        <span class="close" onclick="closeLightbox()">&times;</span>
+
+        <!-- Slides -->
+        <?php foreach ($images as $img): ?>
+            <div class="slide"><img src="<?= $img ?>" class="lightbox-img"></div>
+        <?php endforeach; ?>
+
+        <!-- Navigation -->
+        <a class="prev" onclick="changeSlide(-1)">&#10094;</a>
+        <a class="next" onclick="changeSlide(1)">&#10095;</a>
+    </div>
+
+
+
+    <!-- Navigation -->
+    <a class="prev" onclick="changeSlide(-1)">&#10094;</a>
+    <a class="next" onclick="changeSlide(1)">&#10095;</a>
     </div>
     <!-- Footer Start -->
     <?php
@@ -406,6 +139,32 @@
         $(window).scroll(function() {
             checkSection();
         });
+        let currentSlideIndex = 1;
+
+        function openLightbox() {
+            document.getElementById("lightboxModal").style.display = "block";
+        }
+
+        function closeLightbox() {
+            document.getElementById("lightboxModal").style.display = "none";
+        }
+
+        function showSlide(n) {
+            let slides = document.getElementsByClassName("slide");
+            if (n > slides.length) currentSlideIndex = 1;
+            else if (n < 1) currentSlideIndex = slides.length;
+            else currentSlideIndex = n;
+
+            for (let i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+
+            slides[currentSlideIndex - 1].style.display = "block";
+        }
+
+        function changeSlide(n) {
+            showSlide(currentSlideIndex + n);
+        }
     </script>
 </body>
 
